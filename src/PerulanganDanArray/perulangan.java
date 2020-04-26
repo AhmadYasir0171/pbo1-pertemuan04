37a8e86d39c988c78d13684c093da33b8228ea91
AhmadYasir0171 <ahmadyasir473@gmail.com>
2020-04-26 21:11:01 +0800

    Penerapan Perulangan Do while

--- src/PerulanganDanArray/perulangan.java
+++ src/PerulanganDanArray/perulangan.java
@@ -1,24 +1,27 @@
 package PerulanganDanArray;
 
-  public class perulangan {
+import java.util.Scanner;
+
+public class perulangan {
     public static void main(String[] args) {
-        int koin = 0;
-        int ekor = 0;
-        int kepala = 0;
+        Scanner scanner = new Scanner(System.in);
+        String nama,hobi;
+        char pilih;
+        Integer umur;
 
-        do{
-            koin = (int)(Math.random() * 2);
-            if(koin == 1){
-                System.out.println("Ekor");
-                ekor++;
-                kepala =0;
-            }else{
-                System.out.println("Kepala");
-                kepala++;
-                ekor =0;
-            }
+        do {
+            System.out.print("Masukkan Nama : ");
+            nama = scanner.nextLine();
+            System.out.print("Masukkan Hobi : ");
+            hobi = scanner.nextLine();
+            System.out.print("Masukkam Umur : ");
+            umur = scanner.nextInt();
+            System.out.print("Apakah ingin input ulang? ");
+            pilih = scanner.next().charAt(0);
+        }while(pilih == 'y' && pilih=='Y');
 
-        }while (ekor != 3 && kepala != 3);
-        System.out.println("Program Berhenti");
+        System.out.println("Nama : " + nama);
+        System.out.println("Hobi : " + hobi);
+        System.out.println("Umur : " + umur);
     }
 }
